19f22107d31e8e3959c005b18da616e9c71d5d3d
Rhonda <rlgoolsby@gmail.com>
2026-09-25 21:55:42 +0000

    moving legacy css.stylesheet into src /global.css
    Signed-off-by: Rhonda <rlgoolsby@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+// 1. Load Bootstrap rules first
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// 2. Load your legacy custom CSS second (so your rules win specificity ties)
+import './global.css';
+
+// 3. Load React cores
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
