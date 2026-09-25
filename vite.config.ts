3fabee65cf89e9bd455b01f069989fdcad064ce2
Rhonda <rlgoolsby@gmail.com>
2026-09-25 22:15:39 +0000

    added export defaultConfig to listen to all local addresses so the container can map it
    Signed-off-by: Rhonda <rlgoolsby@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Tells Vite to listen on all local addresses so the container can map it
+    port: 5173
+  }
 })
