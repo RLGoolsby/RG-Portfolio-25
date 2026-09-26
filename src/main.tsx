b6d25fd0f202f119f2af18e79cb452ad2aeb3c13
Rhonda <rlgoolsby@gmail.com>
2026-09-26 21:09:02 +0000

    making file edits and adding features to devcontainer
    Signed-off-by: Rhonda <rlgoolsby@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // 2. Load your legacy custom CSS second (so your rules win specificity ties)
-import './global.css';
+import './src/global.css';
 
 // 3. Load React cores
 import React from 'react';
